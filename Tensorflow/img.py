3e9801285282fcdc98d66bdd6fe6a29f706988b8
lopezjjjj <89049390+lopezjjjj@users.noreply.github.com>
2025-08-30 17:35:54 -0300

    Create img.py

---
+++ Tensorflow/img.py
@@ -0,0 +1,24 @@
+import cv2 #open cv
+import os
+import time
+import uuid
+
+IMAGES_PATH = 'Tensorflow/workspace/images/collectedimages'
+labels = ['hello','thanks','yes','no','iloveyou']
+number_imgs = 15
+
+for label in labels:
+    os.mkdir(r"Tensorflow\\workspace\\images\collectedimages\\"+label)
+    cap = cv2.VideoCapture(0)
+    print('Collecting images for {}'.format(label))
+    time.sleep(5)
+    for imgnum in range (number_imgs):
+        ret,frame = cap.read()
+        imagename = os.path.join(IMAGES_PATH, label, label+'.'+'{}.jpg'.format(str(uuid.uuid1())))
+        cv2.imwrite(imagename,frame)
+        cv2.imshow('frame',frame)
+        time.sleep(2)
+
+        if cv2.waitkey(1) and 0xFF == ord('q'):
+            break
+        cap.release()
